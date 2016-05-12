12b589824e62eea50a9543dc5aa4d9e98763cdc9
Marq Aideron <marq.aideron@gmail.com>
2016-05-12 14:44:23 -0400

    .gitkeeps for EVERYONE!!
    Some minor restructuring with dependencies and where they should live.
    Starting to lay some groundwork for how data is handled.

--- aba-industry/industry-common-bus/src/main/java/com/aba/industry/bus/IndustryDirectorConnection.java
+++ aba-industry/industry-common-bus/src/main/java/com/aba/industry/bus/IndustryDirectorConnection.java
@@ -1,5 +1,7 @@
 package com.aba.industry.bus;
 
 public interface IndustryDirectorConnection {
+	public String requestReply ( String message );
+	
 	void sendMessage ( String topic, Object messageBody );
 }
