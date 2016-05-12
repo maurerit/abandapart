347d2c5995d9a17f48ab304df1eaac4c1a556184
Marq Aideron <marq.aideron@gmail.com>
2016-05-12 14:44:23 -0400

    .gitkeeps for EVERYONE!!
    Some minor restructuring with dependencies and where they should live.
    Starting to lay some groundwork for how data is handled.

---
+++ aba-data/src/test/java/com/aba/industry/data/SampleDataType.java
@@ -0,0 +1,44 @@
+package com.aba.industry.data;
+
+import java.util.Map;
+
+import com.aba.data.TypedJson;
+
+public class SampleDataType extends TypedJson {
+	private String id;
+	private int quantity;
+	private double amount;
+	private Map<String, String> keyValues;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public Map<String, String> getKeyValues() {
+		return keyValues;
+	}
+
+	public void setKeyValues(Map<String, String> keyValues) {
+		this.keyValues = keyValues;
+	}
+}
---
+++ aba-data/src/test/java/com/aba/industry/data/SerializeAndDeserialzeUsingJacksonTests.java
@@ -0,0 +1,43 @@
+package com.aba.industry.data;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class SerializeAndDeserialzeUsingJacksonTests {
+	private final ObjectMapper objectMapper = new ObjectMapper();
+	
+	@Test
+	public void test() throws IOException, ClassNotFoundException {
+		SampleDataType sdt = new SampleDataType();
+		sdt.setId("jfkdajfldkafklsdjflafadsfjkadlsfjasl");
+		sdt.setQuantity(200);
+		sdt.setAmount(50000.01d);
+		
+		Map<String, String> keyValues = new HashMap<>();
+		keyValues.put("Key1", "Value1");
+		keyValues.put("Key2", "Value2");
+		
+		sdt.setKeyValues(keyValues);
+		
+		String json = objectMapper.writeValueAsString(sdt);
+		
+		System.out.println(json);
+		
+		JsonNode jsonNode = objectMapper.readTree(json);
+		
+		String type = jsonNode.get("javaType").asText();
+		
+		Class<?> cls = Class.forName(type);
+		
+		Object o = objectMapper.convertValue(jsonNode, cls);
+		
+		System.out.println(o.getClass());
+	}
+
+}
