f572a2aceb648c7bf30a4b001ee6f517d24a007b
Marq Aideron <marq.aideron@gmail.com>
2016-05-15 12:33:48 -0400

    Industry Director beginnings.

---
+++ aba-data/src/main/java/com/aba/data/bus/CopyableMessage.java
@@ -0,0 +1,12 @@
+package com.aba.data.bus;
+
+import com.aba.data.TypedJsonMessage;
+
+/**
+ * A simple abstract message class that designates that it can be copied onto a publish socket.
+ * 
+ * @author Marq Aideron <marq.aideron@gmail.com>
+ */
+public abstract class CopyableMessage extends TypedJsonMessage {
+
+}
