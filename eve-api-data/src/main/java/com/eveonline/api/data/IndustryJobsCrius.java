f5b470a896a3bf73f2cd0c00dcabed395a01f9df
Shalken <Shalken12@gmail.com>
2016-06-19 08:48:55 -0400

    Step 1. Clean up all the things. Migrated getters and setters to be compacted together.

--- eve-api-data/src/main/java/com/eveonline/api/data/IndustryJobsCrius.java
+++ eve-api-data/src/main/java/com/eveonline/api/data/IndustryJobsCrius.java
@@ -5,7 +5,6 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import java.util.Date;
 
-
 @Entity
 @Table(name = "apiindustryjobscrius")
 public class IndustryJobsCrius
@@ -44,231 +43,175 @@ public class IndustryJobsCrius
     public int getJobID() {
         return jobID;
     }
-
-    public void setJobID(int jobID) {
-        this.jobID = jobID;
-    }
-
     public int getInstallerId() {
         return installerId;
     }
-
-    public void setInstallerId(int installerId) {
-        this.installerId = installerId;
-    }
-
     public String getInstallerName() {
         return installerName;
     }
-
-    public void setInstallerName(String installerName) {
-        this.installerName = installerName;
-    }
-
     public int getFacilityId() {
         return facilityId;
     }
-
-    public void setFacilityId(int facilityId) {
-        this.facilityId = facilityId;
-    }
-
     public int getSolarSystemId() {
         return solarSystemId;
     }
-
-    public void setSolarSystemId(int solarSystemId) {
-        this.solarSystemId = solarSystemId;
-    }
-
     public String getSolarSystemName() {
         return solarSystemName;
     }
-
-    public void setSolarSystemName(String solarSystemName) {
-        this.solarSystemName = solarSystemName;
-    }
-
     public int getStationId() {
         return stationId;
     }
-
-    public void setStationId(int stationId) {
-        this.stationId = stationId;
-    }
-
     public int getActivityId() {
         return activityId;
     }
-
-    public void setActivityId(int activityId) {
-        this.activityId = activityId;
-    }
-
     public int getBlueprintId() {
         return blueprintId;
     }
-
-    public void setBlueprintId(int blueprintId) {
-        this.blueprintId = blueprintId;
-    }
-
     public int getBlueprintTypeId() {
         return blueprintTypeId;
     }
-
-    public void setBlueprintTypeId(int blueprintTypeId) {
-        this.blueprintTypeId = blueprintTypeId;
-    }
-
     public String getBlueprintTypeName() {
         return blueprintTypeName;
     }
-
-    public void setBlueprintTypeName(String blueprintTypeName) {
-        this.blueprintTypeName = blueprintTypeName;
-    }
-
     public int getBlueprintLocationId() {
         return blueprintLocationId;
     }
-
-    public void setBlueprintLocationId(int blueprintLocationId) {
-        this.blueprintLocationId = blueprintLocationId;
-    }
-
     public int getOutputLocationId() {
         return outputLocationId;
     }
-
-    public void setOutputLocationId(int outputLocationId) {
-        this.outputLocationId = outputLocationId;
-    }
-
     public int getRuns() {
         return runs;
     }
-
-    public void setRuns(int runs) {
-        this.runs = runs;
-    }
-
     public double getCost() {
         return cost;
     }
-
-    public void setCost(double cost) {
-        this.cost = cost;
-    }
-
     public int getTeamId() {
         return teamId;
     }
-
-    public void setTeamId(int teamId) {
-        this.teamId = teamId;
-    }
-
     public int getLicensedRuns() {
         return licensedRuns;
     }
-
-    public void setLicensedRuns(int licensedRuns) {
-        this.licensedRuns = licensedRuns;
-    }
-
     public double getProbability() {
         return probability;
     }
-
-    public void setProbability(double probability) {
-        this.probability = probability;
-    }
-
     public int getProductTypeId() {
         return productTypeId;
     }
-
-    public void setProductTypeId(int productTypeId) {
-        this.productTypeId = productTypeId;
-    }
-
     public String getProductTypeName() {
         return productTypeName;
     }
-
-    public void setProductTypeName(String productTypeName) {
-        this.productTypeName = productTypeName;
-    }
-
     public int getStatus() {
         return status;
     }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
     public int getTimeInSeconds() {
         return timeInSeconds;
     }
-
-    public void setTimeInSeconds(int timeInSeconds) {
-        this.timeInSeconds = timeInSeconds;
-    }
-
     public Date getStartDate() {
         return startDate;
     }
-
-    public void setStartDate(Date startDate) {
-        this.startDate = startDate;
-    }
-
     public Date getEndDate() {
         return endDate;
     }
-
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
-    }
-
     public Date getPauseDate() {
         return pauseDate;
     }
-
-    public void setPauseDate(Date pauseDate) {
-        this.pauseDate = pauseDate;
-    }
-
     public Date getCompletedDate() {
         return completedDate;
     }
-
-    public void setCompletedDate(Date completedDate) {
-        this.completedDate = completedDate;
-    }
-
     public int getCompletedCharacterId() {
         return completedCharacterId;
     }
-
-    public void setCompletedCharacterId(int completedCharacterId) {
-        this.completedCharacterId = completedCharacterId;
-    }
-
     public int getSuccessfulRuns() {
         return successfulRuns;
     }
-
-    public void setSuccessfulRuns(int successfulRuns) {
-        this.successfulRuns = successfulRuns;
-    }
-
     public int getCorporationId() {
         return corporationId;
     }
 
+    public void setJobID(int jobID) {
+        this.jobID = jobID;
+    }
+    public void setInstallerId(int installerId) {
+        this.installerId = installerId;
+    }
+    public void setInstallerName(String installerName) {
+        this.installerName = installerName;
+    }
+    public void setFacilityId(int facilityId) {
+        this.facilityId = facilityId;
+    }
+    public void setSolarSystemId(int solarSystemId) {
+        this.solarSystemId = solarSystemId;
+    }
+    public void setSolarSystemName(String solarSystemName) {
+        this.solarSystemName = solarSystemName;
+    }
+    public void setStationId(int stationId) {
+        this.stationId = stationId;
+    }
+    public void setActivityId(int activityId) {
+        this.activityId = activityId;
+    }
+    public void setBlueprintId(int blueprintId) {
+        this.blueprintId = blueprintId;
+    }
+    public void setBlueprintTypeId(int blueprintTypeId) {
+        this.blueprintTypeId = blueprintTypeId;
+    }
+    public void setBlueprintTypeName(String blueprintTypeName) {
+        this.blueprintTypeName = blueprintTypeName;
+    }
+    public void setBlueprintLocationId(int blueprintLocationId) {
+        this.blueprintLocationId = blueprintLocationId;
+    }
+    public void setOutputLocationId(int outputLocationId) {
+        this.outputLocationId = outputLocationId;
+    }
+    public void setRuns(int runs) {
+        this.runs = runs;
+    }
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+    public void setTeamId(int teamId) {
+        this.teamId = teamId;
+    }
+    public void setLicensedRuns(int licensedRuns) {
+        this.licensedRuns = licensedRuns;
+    }
+    public void setProbability(double probability) {
+        this.probability = probability;
+    }
+    public void setProductTypeId(int productTypeId) {
+        this.productTypeId = productTypeId;
+    }
+    public void setProductTypeName(String productTypeName) {
+        this.productTypeName = productTypeName;
+    }
+    public void setStatus(int status) {
+        this.status = status;
+    }
+    public void setTimeInSeconds(int timeInSeconds) {
+        this.timeInSeconds = timeInSeconds;
+    }
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+    public void setPauseDate(Date pauseDate) {
+        this.pauseDate = pauseDate;
+    }
+    public void setCompletedDate(Date completedDate) {
+        this.completedDate = completedDate;
+    }
+    public void setCompletedCharacterId(int completedCharacterId) {
+        this.completedCharacterId = completedCharacterId;
+    }
+    public void setSuccessfulRuns(int successfulRuns) {
+        this.successfulRuns = successfulRuns;
+    }
     public void setCorporationId(int corporationId) {
         this.corporationId = corporationId;
     }
