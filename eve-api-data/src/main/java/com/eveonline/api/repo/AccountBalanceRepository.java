06df3c7411707e3222ca8b7d7bf505466178ba13
Shalken <Shalken12@gmail.com>
2016-06-19 15:22:34 -0400

    Test passed!

--- eve-api-data/src/main/java/com/eveonline/api/repo/AccountBalanceRepository.java
+++ eve-api-data/src/main/java/com/eveonline/api/repo/AccountBalanceRepository.java
@@ -3,6 +3,12 @@ package com.eveonline.api.repo;
 
 import com.eveonline.api.data.AccountBalance;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface AccountBalanceRepository extends JpaRepository<AccountBalance, Long> {
+public interface AccountBalanceRepository extends JpaRepository<AccountBalance, Long>
+{
+    @Query(value = "select a from AccountBalance a where a.accountKey = :accountKey and a.corporationId = :corporationId")
+    AccountBalance findByAccountKeyAndCorporationId(@Param("accountKey")int a, @Param("corporationId")int c);
+
 }
