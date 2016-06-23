dbd3e87d3ca7d59e2fefd1b08228eacd5001a9ce
Marq Aideron <marq.aideron@gmail.com>
2016-06-22 21:50:14 -0400

    Client Model stuff and more
    Starting to flesh out the client models to abstract out the wonkiness that is the api data model
    Brought in a couple links for libraries that are not on maven central and are managed by gradle builds

---
+++ eve-api-data/src/main/java/com/eveonline/api/data/HangerDivision.java
@@ -0,0 +1,5 @@
+package com.eveonline.api.data;
+
+public class HangerDivision {
+
+}
---
+++ eve-api-data/src/main/java/com/eveonline/api/model/DivisionWallet.java
@@ -0,0 +1,5 @@
+package com.eveonline.api.model;
+
+public class DivisionWallet {
+	private Division division;
+}
