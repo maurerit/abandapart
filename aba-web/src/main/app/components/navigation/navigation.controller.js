df18b8ea514c19398ba69495799da72071b23b4c
Marq Aideron <marq.aideron@gmail.com>
2016-06-30 12:38:06 -0400

    Navigation moving along
    The navigation now renders and displays which one is active

--- aba-web/src/main/app/components/navigation/navigation.controller.js
+++ aba-web/src/main/app/components/navigation/navigation.controller.js
@@ -1,7 +1,27 @@
 class NavigationController {
-  constructor() {
-    this.name = 'navigation';
-  }
+	constructor() {
+		this.name = 'navigation';
+
+		//TODO: I think I want this to be gathered from the backend but I could be an idiot?
+		this.navItems = [
+			{
+				"link": "/home",
+				"class": "active",
+				"name": "Home"
+			},
+			{
+				"link": "/about",
+				"class": "none",
+				"name": "About"
+			}
+		];
+
+		this.activeItem = this.navItems[0];
+	}
+
+	click ( navItem ) {
+		this.activeItem = navItem;
+	}
 }
 
 export default NavigationController;
