d63fb8c19b66b1603ba073e631f7ac2eb6c8d49f
Marq Aideron <marq.aideron@gmail.com>
2016-07-06 17:01:07 -0400

    Angular Chart.js almost working
    Its adding the wrapper div but it appears that the bindings are not properly happening... almost there I think

--- aba-web/src/main/app/components/wallet/wallet.controller.js
+++ aba-web/src/main/app/components/wallet/wallet.controller.js
@@ -1,6 +1,26 @@
 class WalletController {
   constructor() {
     this.name = 'wallet';
+	this.someData = {
+		labels: [
+        'Apr', 
+        'May', 
+        'Jun'
+      ],
+      datasets: [
+        {
+          data: [1, 7, 15, 19, 31, 40]
+        },
+        {
+          data: [6, 12, 18, 24, 30, 36]
+        }
+      ]
+	};
+
+	this.someOptions = {
+		segementStrokeWidth: 20,
+		segmentStrokeColor: '#000'
+	};
   }
 }
 
--- aba-web/src/main/app/components/wallet/wallet.js
+++ aba-web/src/main/app/components/wallet/wallet.js
@@ -2,16 +2,30 @@ import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import walletComponent from './wallet.component';
 
+import chartJs from 'chart.js';
+import angularCharts from 'angular-chart.js';
+
 let walletModule = angular.module('wallet', [
-  uiRouter
+  uiRouter,
+  'chart.js'
 ])
-.config(/*@ngInject*/($stateProvider, $urlRouterProvider) => {
+.config(/*@ngInject*/($stateProvider, $urlRouterProvider, ChartJsProvider) => {
   $urlRouterProvider.otherwise('/');
 
   $stateProvider
     .state('wallet', {
       url: '/wallet',
       template: '<wallet></wallet>'
+    });
+
+    // Configure all charts
+    ChartJsProvider.setOptions({
+      colours: ['#97BBCD', '#DCDCDC', '#F7464A', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'],
+      responsive: true
+    });
+    // Configure all doughnut charts
+    ChartJsProvider.setOptions('Doughnut', {
+      animateScale: true
     });
 })
 .component('wallet', walletComponent);
