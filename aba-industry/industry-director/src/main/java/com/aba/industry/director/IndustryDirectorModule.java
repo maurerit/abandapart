0200d80c2e706a9433706b4bb837920250e989d5
Marq Aideron <marq.aideron@gmail.com>
2016-07-09 01:59:49 -0400

    Web Tests failing...
    Cannot find the components/wallet/salesgraph/salesgraph component... I thought I had those unit tests passing?
    Will investigate later
    
    Added in some backings for temporarily gathering blueprint related data.  I hope fuzzy steve does not hate me but I will be
    be a nice consumer and cache his data for quite a long time to reduce throughput on this server.  I really should talk to him
    to see if he is ok with my consuming the data he has available for his web site.

--- aba-industry/industry-director/src/main/java/com/aba/industry/director/IndustryDirectorModule.java
+++ aba-industry/industry-director/src/main/java/com/aba/industry/director/IndustryDirectorModule.java
@@ -7,6 +7,7 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class IndustryDirectorModule {
 	public static void main ( String[ ] args ) {
+		@SuppressWarnings("unused")
 		ApplicationContext ctx = SpringApplication.run(IndustryDirectorModule.class, args);
 	}
 }
--- eve-api-data/src/main/java/com/eveonline/api/repo/AccountBalanceRepository.java
+++ eve-api-data/src/main/java/com/eveonline/api/repo/AccountBalanceRepository.java
@@ -1,10 +1,8 @@
 package com.eveonline.api.repo;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.eveonline.api.data.AccountBalance;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface AccountBalanceRepository extends JpaRepository<AccountBalance, Long>
 {
