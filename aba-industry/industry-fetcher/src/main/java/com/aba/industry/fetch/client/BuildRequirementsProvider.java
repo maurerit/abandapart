f24112520d6cb5d708bbe2519eaaeaca7d55b9ae
Matthew L. Maurer <matt.maurer@kroger.com>
2016-07-15 07:22:32 -0400

    Wallet Refactoring
    Remove the wallet subcomponents.  A better understanding of angular directives along with not wanting to have sub components that do the same thing.  I was thinking the isolation was necessary or wanted but it just convolutes things.

--- aba-industry/industry-fetcher/src/main/java/com/aba/industry/fetch/client/BuildRequirementsProvider.java
+++ aba-industry/industry-fetcher/src/main/java/com/aba/industry/fetch/client/BuildRequirementsProvider.java
@@ -8,5 +8,17 @@ import java.io.IOException;
  * @author maurerit
  */
 public interface BuildRequirementsProvider {
+    /**
+     * This method originate from Fuzzy Steve's blueprint calculator and how it gathers
+     * the data it needs.  The data structure was quite nice and included everything that is
+     * needed to determine all the times and materials along with skills needed.  Sort
+     * of a one stop shop so I just stole the structure and (at least temporarily) will consume
+     * his data until I have the time to create my own.
+     *
+     * @param typeId
+     * @return A {@link BlueprintData} object instance or null if nothing was found.
+     * @throws IOException This method consumes data from the web and could run into issue's
+     * be prepared.
+     */
     BlueprintData getBlueprintData ( Long typeId ) throws IOException;
 }
