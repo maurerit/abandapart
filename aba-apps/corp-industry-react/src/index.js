7021bfdf136b5897696ca37edd904490dd9eefbe
Marq Aideron <marq.aideron@gmail.com>
2017-03-04 13:57:00 -0500

    Revert: #15 Various Workings

--- aba-apps/corp-industry-react/src/index.js
+++ aba-apps/corp-industry-react/src/index.js
@@ -1,19 +1,27 @@
-/*
- * Copyright 2017 maurerit
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
- */
-
 import React from "react";
 import ReactDOM from "react-dom";
+import {Router, Route, IndexRoute, hashHistory} from "react-router";
 import App from "./App";
+import Home from './components/Home';
+import Tasks from './components/Tasks';
+import Producers from './components/producers/Producers';
+import Buildables from './components/Buildables';
 import "./index.css";
 
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
 ReactDOM.render(
-    <App />,
+    <Router history={hashHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={Home} />
+            <Route path="/tasks" component={Tasks}/>
+            <Route path="/producers" component={Producers}/>
+            <Route path="/buildables" component={Buildables} />
+        </Route>
+    </Router>,
     document.getElementById('root')
 );
